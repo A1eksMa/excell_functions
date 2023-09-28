e3c213ccd19127081f1172e6509067c4d0242d00
NieR <a1ex_ma@mail.ru>
2023-09-28 16:45:53 +0300

    Add bash-script with categories

---
+++ structure.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create list of categories
+list=("compatibility" "cube" "database" "datetime" "engineering" "financial" "information" "lookup" "math" "statistical" "text" "users" "web")
+
+# Make folders
+for folder in "${list[@]}"
+do
+    mkdir "$folder"
+    cd "$folder"
+    str="import "
+    str+="$folder"
+    echo "$str" >>__init__.py
+    touch "$folder"".py"
+    cd ../
+done
