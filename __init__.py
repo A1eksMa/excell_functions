6bc50b9b068e95e3b1f63a0965c9548394e18481
A1eksMa <145852894+A1eksMa@users.noreply.github.com>
2023-12-06 00:35:22 +0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,24 +1,18 @@
-import math
-import numpy as np
-import pandas as pd
-import datetime as dt
-
-import warnings
-warnings.filterwarnings('ignore')
-
 '''
-from excell_functions import comptibility
-from excell_functions import cube
-from excell_functions import database
-from excell_functions import datetime
-from excell_functions import engineering
-from excell_functions import financial
-from excell_functions import information
-from excell_functions import logical
-from excell_functions import lookup
-from excell_functions import math
-from excell_functions import statistical
-from excell_functions import text
-from excell_functions import users
-from excell_functions import web
+This library contains modules:
+
+comptibility
+cube
+database
+datetime
+engineering
+financial
+information
+logical
+lookup
+math
+statistical
+text
+users
+web
 '''
