fb4aa1b0ba3355d81bf3aa28ef24030dfe4a70f8
root <root@194-67-121-15.cloudvps.regruhosting.ru>
2023-12-06 12:31:05 +0300

    Add import modules in __init__.py

--- excell_functions/__init__.py
+++ excell_functions/__init__.py
@@ -1,18 +1,14 @@
-'''
-This library contains modules:
-
-comptibility
-cube
-database
-datetime
-engineering
-financial
-information
-logical
-lookup
-math
-statistical
-text
-users
-web
-'''
+from .comptibility import *
+from .cube import *
+from .database import *
+from .datetime import *
+from .engineering import *
+from .financial import *
+from .information import *
+from .logical import *
+from .lookup import *
+from .math import *
+from .statistical import *
+from .text import *
+from .users import *
+from .web import *
